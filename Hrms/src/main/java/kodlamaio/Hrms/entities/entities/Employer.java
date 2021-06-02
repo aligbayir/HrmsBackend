e5e22633352d6d5d7dc87df4d947738308704e1e
Ali Görkem Bayır <a.bayrgorkem@gmail.com>
2021-06-02 14:45:11 +0300

    deneme

--- Hrms/src/main/java/kodlamaio/Hrms/entities/entities/Employer.java
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/entities/Employer.java
@@ -4,5 +4,9 @@ public class Employer {
 	private int id;
 	private String companyName;
 	private String companyWebSite;
-	private String companyMail;
+	private String email;
+	private String phoneNumber;
+	private String password;
+	private String Status;
+	
 }
