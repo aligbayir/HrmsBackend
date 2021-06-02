45f4ee8565b5de3890f3f748fa5f9ef21a63207f
Ali Görkem Bayır <a.bayrgorkem@gmail.com>
2021-06-02 14:24:54 +0300

    Hrms

---
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/entities/Employer.java
@@ -0,0 +1,8 @@
+package kodlamaio.Hrms.entities.entities;
+
+public class Employer {
+	private int id;
+	private String companyName;
+	private String companyWebSite;
+	private String companyMail;
+}
